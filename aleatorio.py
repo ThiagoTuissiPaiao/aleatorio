eacc3e09fd67116ddcfe73a53d4c74b80950353b
Thiago Tuissi Paião <81596921+ThiagoTuissiPaiao@users.noreply.github.com>
2021-04-12 22:56:51 -0300

    Add files via upload

---
+++ aleatorio.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+print("Sorteando em 3...")
+time.sleep(1)
+print("Sorteando em 2...")
+time.sleep(1)
+print("Sorteando em 1...")
+time.sleep(1)
+
+print(f"O aluno sorteado é: {random.randrange(1, 65)}")
+
+
